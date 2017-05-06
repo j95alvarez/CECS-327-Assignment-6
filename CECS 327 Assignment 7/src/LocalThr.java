c3ab93cb3b89ef67c2a286a18ee3243959f2caf4
devinjoseph <devin.joseph@yahoo.com>
2017-05-06 12:35:28 -0700

    localThr
    The localThr and some changes to the uThr.

--- CECS 327 Assignment 7/src/LocalThr.java
+++ CECS 327 Assignment 7/src/LocalThr.java
@@ -1,5 +1,29 @@
+import java.util.*;
 public class LocalThr extends Thread {
+	int x = 0;
+	
 	public void run() {
+		if(x > 10){
+			x = 0;
+		}
+		//if call == nextEven
+			//setNextEven()
+		//if call == nextOdd
+			//setNextOdd()
 		System.out.println("Hello");
+	}
+	
+	public void setNextEven(){
+		while(x % 2 != 0){
+			x++;
+		}
+		//put x in queue if even
+	}
+	
+	public void setNextOdd(){
+		while(x % 2 == 0){
+			x++
+		}
+		//put x in queue if odd
 	}
 }
