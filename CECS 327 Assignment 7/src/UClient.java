724679d109681c514f9ab20e7e0b4435fa2ac5b8
j95alvarez <juanalvarez391@yahoo.com>
2017-05-10 10:49:59 -0700

    Finished

--- CECS 327 Assignment 7/src/UClient.java
+++ CECS 327 Assignment 7/src/UClient.java
@@ -1,25 +1,23 @@
 import java.util.concurrent.*;
 
+
 public class UClient {
 	public static void main(String[] args) {
 		// Creates 2 concurrent queue objects
 		ConcurrentLinkedQueue<Node> requestQue = new ConcurrentLinkedQueue<Node>();
 		ConcurrentLinkedQueue<Node> resultQue = new ConcurrentLinkedQueue<Node>();
 		
-		// Creates the RuntimeThr and initialize an arrary to hold 8 UThr
-		Thread t1 = new Thread(new RuntimeThr(requestQue, resultQue), "Main");	
-		Thread[] ts = new Thread[8];
+		try {
+			// Creates the RuntimeThr and initialize an arrary to hold 8 UThr
+			Thread t1 = new Thread(new RuntimeThr(requestQue, resultQue));	
+			Thread[] ts = new Thread[8];
 
-		// Starts the RuntimeThr
-		t1.start();
-
-		// Creates the UThr
-		for(int i = 0; i < ts.length; i++)
-			ts[i] = new Thread(new UThr(i, requestQue, resultQue), i + "");
-
-		// Starts the UThr
-		for(int i = 0; i < ts.length; i++)
-			ts[i].start();
-
+			// Starts the RuntimeThr
+			t1.start();
+			t1.join();
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+		
 	}
 }
--- CECS 327 Assignment 7/src/UThr.java
+++ CECS 327 Assignment 7/src/UThr.java
@@ -2,11 +2,14 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.*;
 import java.util.*;
+import java.util.concurrent.locks.*;
 
 public class UThr implements Runnable {
 	public int id;
 	public ConcurrentLinkedQueue<Node> requestQue;
 	public ConcurrentLinkedQueue<Node> resultQue;
+	public boolean done = false;
+	public static Lock lock = new ReentrantLock();
 
 	public UThr(int i, ConcurrentLinkedQueue<Node> request, ConcurrentLinkedQueue<Node> result){
 		this.id = i;
@@ -23,31 +26,50 @@ public class UThr implements Runnable {
 			String request = "";
 
 			//Chooses random number from 0 - 4
-			if (x == 0){
+			if (x == 0)
 				//If x = 0, go to nextEven thread
 				request = "NEXTEVEN";
-			}
-			if (x == 1){
+			if (x == 1)
 				//If x = 1, go to nextOdd thread
 				request = "NEXTODD";
-			}
-			if (x == 2){
+			if (x == 2)
 				//If x = 2, go to nextEvenFib thread
 				request = "NEXTEVENFIB";
-			}	
-			if (x == 3){
+			if (x == 3)
 				//If x = 3, go to nextLargerRand thread
 				request = "NEXTLARGERRAND";
-			}
-			if (x == 4){
+			if (x == 4)
 				//If x = 4, go to nextPrime thread
 				request = "NEXTPRIME";
-
-			}
 			
 			Node temp = new Node(id, i, request);
 
 			requestQue.add(temp);
+
+			// Loops until the result queue is no longer
+			// empty and then prints it out using locks
+			while(!done){
+				lock.lock();
+				try {
+					// Tests to see if the result queue is not
+					// empty in order to print
+					if(!resultQue.isEmpty())
+						if(resultQue.peek() != null) {
+							// Test to see if the result from 
+							// the request that was generated
+							// from this iteration
+							if(resultQue.peek().id == id) {
+								if(resultQue.peek().cmdNum == i) {
+									System.out.println(resultQue.poll());
+									break;
+								}
+							}
+							
+						}
+				} finally {
+					lock.unlock();
+				}
+			}
 		}
 	}
 }
